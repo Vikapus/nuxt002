5ed26b5c67fd693b8b6e1a905bd3d0ea26c02880
vikap <vikakapushak@yandex.ru>
2024-02-06 02:49:51 +0300

    step_1

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+    <section>
+        <h1>About page</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis reiciendis aliquam quasi? Iure magnam alias, sequi minima quo, tempore ad qui ut adipisci maiores magni. Reiciendis architecto culpa facilis quod.</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis reiciendis aliquam quasi?</p>
+
+    </section>
+
+</template>
+
+<script>
+export default {
+    middleware: ['auth']
+}
+</script>
